938baf790074edbf96e1f1b39ecf3b6c4e0773a6
XGuppy <xguppy@yandex.ru>
2018-10-19 19:19:53 +0400

    d3

--- TG/src/main.ts
+++ TG/src/main.ts
@@ -1,3 +1,20 @@
 import * as d3 from 'd3'
+var height :number = 500;
+var width: number = 500;
 
-alert(d3.version);
+var margin: number = 25;
+var offset: number = 50;
+
+var axisWidth: number = width - 2* margin;
+var svg;
+createAxes();
+
+function createAxes(): void
+{
+    svg = d3.select("body").append("svg")
+                           .attr("class", "axis")
+                           .attr("width", width)
+                           .attr("height", height);
+    // функция интерполяции времени на оси
+    var scale = d3.timeFormat
+}
