53c499d787dac9a27da67635312a7927c8122bcd
macbook <dikuz01@mail.ru>
2018-10-26 21:35:24 +0400

    Add "point" redraw

--- TG/src/main.ts
+++ TG/src/main.ts
@@ -1,8 +1,8 @@
 import * as d3 from 'd3'
-import {Resource} from './resource'
+import {Resource, Task} from './resource'
 
-//Определяем размеры и пространство вывода svg 
-const width = 960;
+//Определяем размеры и пространство вывода svg
+const width = 1200;
 const height = 480;
 
 let svg = d3.select('body')
@@ -10,7 +10,7 @@ let svg = d3.select('body')
     .attr('width', width)
     .attr('height', height);
 
- //Отступы от графика
+//Отступы от графика
 let plotMargins = {
     top: 30,
     bottom: 30,
@@ -21,7 +21,7 @@ let plotMargins = {
 let plotGroup = svg.append('g')
     .classed('plot', true)
     .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
- // Ширина
+// Ширина
 let plotWidth = width - plotMargins.left - plotMargins.right;
 // Высота
 let plotHeight = height - plotMargins.top - plotMargins.bottom;
@@ -29,11 +29,11 @@ let plotHeight = height - plotMargins.top - plotMargins.bottom;
 let xScale = d3.scaleTime()
     .range([0, plotWidth]);
 //Опредлим ось X, зададим 24 тика на оси от 00 часов до 00 часов
-let xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%H")).ticks(25);
+let xAxis = d3.axisTop(xScale).tickFormat(d3.timeFormat("%H:%M")).ticks(25);
 let xAxisGroup = plotGroup.append('g')
     .classed('x', true)
     .classed('axis', true)
-    .attr('transform', `translate(${0},${plotHeight})`)
+    .attr('transform', `translate(${0},${0})`)
     .call(xAxis);
 //Аналогично для оси ресурсов
 let yScale = d3.scaleBand()
@@ -44,19 +44,37 @@ let yAxisGroup = plotGroup.append('g')
     .classed('axis', true)
     .call(yAxis);
 
-    d3.json<Resource[]>('tasks.json').then((data)=>
+let pointsGroup = plotGroup.append('g')
+    .classed('points', true);
+
+d3.json<Resource[]>('tasks.json').then((data)=>
     {
-            //Массив ресурсов
-            let prepared = data.map(d => {
-                let obj: Resource = {name: d.name, tasks: d.tasks}  //Соберем ресурс
-                return {
-                    Resource : obj
-                }
-                });
-                yScale.domain(prepared.map(d => d.Resource.name));  //Добавил на ось
-                yAxisGroup.call(yAxis); //Обновим ось
-                //Тут будем рисовать "точки" из d.Resource.tasks
-    }, 
+        yScale.domain(data.map(d => d.name));  //Добавил на ось
+        yAxisGroup.call(yAxis); //Обновим ось
+
+        //Тут будем рисовать "точки" из d.Resource.tasks
+
+        var dataBound = pointsGroup.selectAll('.post') //
+            .data(data);
+
+        console.log(data);
+
+        dataBound //Удалим повторяющие точки (вроде не надо исходя из условий задачи)
+            .exit()
+            .remove();
+
+        var enterSelection = dataBound //Добавим точки
+            .enter()
+            .append('g')
+            .classed('post', true);
+
+        enterSelection.append('circle') // Стиль "точки"
+            .attr('r', 2)
+            .style('fill', 'red');
+
+        enterSelection.merge(dataBound) //Обновим все точки и отрисуем на графике
+            .attr('transform', (d, i) => `translate(${xScale(d.tasks[i].start.getTime())},${yScale(d.name)})`);
+    },
     (error) =>      //Если ошибка
     {
         console.log(error);
